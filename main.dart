151350bc9f7d4f7eab6b7ded9eb50c3367e90616
Kan <secureis@gmail.com>
2024-03-04 23:05:20 +0900

    Add comments explaining the importance of the main function in Dart code

--- main.dart
+++ main.dart
@@ -1,3 +1,8 @@
 void main(){
   print("Hello World!");
 }
+
+// main 함수는 모든 dart 프로그램의 entry point이 때문에 아주 중요하다.
+// main이 없을 경우엔 run이 불가능하면 오류가 발생하게 된다.
+// ; (semicolon)은 문장의 끝을 나타내는데, 기본적으로 반드시 끝에 들어가야 한다.
+// 일부러 ;를 빼 먹게 구성하는 것이 따로 있는데, 우선은 꼭 semi-colon을 사용한다고 생각하자.
